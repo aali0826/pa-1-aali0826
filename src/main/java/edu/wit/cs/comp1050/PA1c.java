0d76a53cb1210ebf7c24bd65827d8153aeab95aa
aali0826 <alia6@wit.edu>
2023-01-26 22:36:15 -0500

    Done part A

--- src/main/java/edu/wit/cs/comp1050/PA1c.java
+++ src/main/java/edu/wit/cs/comp1050/PA1c.java
@@ -1,5 +1,7 @@
 package edu.wit.cs.comp1050;
 
+import java.util.Scanner;
+
 //TODO: document this class
 public class PA1c {
 	
@@ -21,7 +23,63 @@ public class PA1c {
 
 	// TODO: document this method
 	public static void main(String[] args) {
-		// TODO: write your code here
+		int quarters = 25;
+		int dimes = 10;
+		int nickels = 5;
+		int pennies = 1;
+
+
+		Scanner sc = new Scanner(System.in);
+
+		String q = "quarters";
+		String d = "dimes";
+		String n = "nickes";
+		String p = "pennies";
+
+		System.out.print("Enter total amount: $ ");
+		double dollars = sc.nextDouble();
+
+		dollars = dollars*100;
+		int dollarsInt = convertToInt(dollars);
+		int numquarters = 0, numdimes = 0, numnickels = 0, numpennies = 0;
+
+		if(dollarsInt >=0) {
+			while (dollarsInt >= quarters) {
+				dollarsInt = dollarsInt - quarters;
+				numquarters++;
+			}
+			while (dollarsInt >= dimes) {
+				dollarsInt = dollarsInt - dimes;
+				numdimes++;
+			}
+			while(dollarsInt >= nickels) {
+				dollarsInt = dollarsInt - nickels;
+				numnickels++;
+			}
+			while(dollarsInt >= pennies) {
+				dollarsInt = dollarsInt - pennies;
+				numpennies++;
+			}
+			if (numquarters == 1) {
+				q = "quarters";
+			}
+			if (numdimes == 1) {
+				d = "dimes";
+			}
+			if (numnickels == 1) {
+				n = "nickels";
+			}
+			if (numpennies == 1) {
+				p = "pennies";
+			}
+			System.out.printf("You have %s %s, %s %s, %s %s, and %s, %s.%n", numquarters, q, numdimes, d, numnickels, n, numpennies, p);
+		}
+		else {
+			System.out.printf("Error");
+		}
+
+
 	}
+
 
 }
