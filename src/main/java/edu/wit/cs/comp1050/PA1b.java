afaf81bbcb023ba09ae9b92ea34aab3d470afd85
aali0826 <alia6@wit.edu>
2023-01-31 19:32:08 -0500

    Done part A

--- src/main/java/edu/wit/cs/comp1050/PA1b.java
+++ src/main/java/edu/wit/cs/comp1050/PA1b.java
@@ -18,14 +18,18 @@ public class PA1b {
 		int pennies;
 
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter number of quarters: Enter number of dimes: Enter number of nickels: Enter number of pennies: You have $0.00 in coins.");
+		System.out.printf("Enter number of quarters: ");
 		quarters = input.nextInt();
+		System.out.printf("Enter number of dimes: ");
 		dimes = input.nextInt();
+		System.out.printf("Enter number of nickels: ");
 		nickels = input.nextInt();
+		System.out.printf("Enter number of pennies: ");
 		pennies = input.nextInt();
 
 
 		double total = ((0.25 * quarters) + (0.10 * dimes) + (0.05 * nickels) + (0.01 * pennies));
+		System.out.printf("You have $%.2f in coins.%n", total);
 	}
 
 
